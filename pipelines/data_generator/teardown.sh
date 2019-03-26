bed99fef1121a185088aa1ee3a5e25edc3856d2c
Quincy Wofford <quincy.wofford@gmail.com>
2019-03-25 18:09:29 -0600

    need to cleanup teardown eventially

--- pipelines/data_generator/teardown.sh
+++ pipelines/data_generator/teardown.sh
@@ -5,8 +5,6 @@ teardown/complain_headnode.sh
 # [wf] Check if ran on headnode
 if [[ $(hostname) == "wheeler-sn" ]]; then 
   # [wf] Complain about running on the headnode one more time
-  teardown/complain_headnode.sh
 else 
   # [wf] Congratulate user for not running on headnode
-  teardown/celebrate_noheadnode.sh
 fi;
