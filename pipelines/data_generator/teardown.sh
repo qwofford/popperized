e17a3f7b08463101a2396c6b0635ac8db65a7c08
Quincy Wofford <quincy.wofford@gmail.com>
2019-03-25 18:14:10 -0600

    implement teardown later

--- pipelines/data_generator/teardown.sh
+++ pipelines/data_generator/teardown.sh
@@ -1,9 +1,1 @@
 #!/usr/bin/env bash
-# [wf] Complain about running on the headnode one more time
-
-# [wf] Check if ran on headnode
-if [[ $(hostname) == "wheeler-sn" ]]; then 
-  # [wf] Complain about running on the headnode one more time
-else 
-  # [wf] Congratulate user for not running on headnode
-fi;
