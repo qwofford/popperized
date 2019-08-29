741a4a119f2a772fcb09870affd1008ece7e5368
Patrick G. Bridges <patrickb@unm.edu>
2019-08-29 13:29:04 -0600

    Fixed teardown.sh

--- pipelines/data_generator/teardown.sh
+++ pipelines/data_generator/teardown.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-TMPDIR=$(cat /wheeler/scratch/qwofford/TMPDIR)
-rm /wheeler/scratch/qwofford/TMPDIR
-rmdir $TMPDIR
+TMPDIR=$(cat /wheeler/scratch/${USER}/TMPDIR)
+if [ -e ${TMPDIR} ]; then
+    rm -r ${TMPDIR}
+fi
+rm /wheeler/scratch/${USER}/TMPDIR
