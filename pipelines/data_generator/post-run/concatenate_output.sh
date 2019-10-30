7a1124c917318313e5c47135b44cbbf4813d9976
Patrick Widener <patrick.widener@sandia.gov>
2019-10-30 12:12:13 -0600

    Initial cut at working with JSON output. Cooperates with bsp_prototype json branch. May not compile, not tried on wheeler or other cluster platform yet

--- pipelines/data_generator/post-run/concatenate_output.sh
+++ pipelines/data_generator/post-run/concatenate_output.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat results/bsp_prototype.header > results/result.csv
+#cat results/bsp_prototype.header > results/result.csv
 TMPDIR=$(cat /wheeler/scratch/${USER}/TMPDIR)
-paste -d'\n' results/job_output/* >> results/result.csv
+paste -d'\n' results/job_output/* >> results/result.json
 rm results/job_output/*
