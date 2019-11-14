d21f43a4cbd4833ffdffad74c33318e240aeba7a
Quincy Wofford <qwofford@wheeler-sn.alliance.unm.edu>
2019-11-14 16:19:33 -0700

    launch ldms agg outside of container for now

--- pipelines/data_generator/run.sh
+++ pipelines/data_generator/run.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+# [wf] Start LDMS aggregator
+export MODULEFILES=/opt/spack/share/spack/modules/linux-centos7-x86_64:/opt/local/modules/all:/opt/ohpc/pub/modulefiles:/wheeler/scratch/qwofford/modulefiles
+module load ovis/4.2.3
+source /wheeler/scratch/qwofford/opt/ovis/4.2.3/configs/start_agg_csv.sh
+
 # [wf] Run dummy simulation
 qsub -I -x -q debug -l nodes=1:ppn=8 -l walltime=0:10:00 -S /bin/bash -j oe -N cdse_datagen ${PWD}/run/datagen.sh
