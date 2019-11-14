a8727b8efd28455aaa9543131a4d4416ce39f1dc
Quincy Wofford <qwofford@wheeler-sn.alliance.unm.edu>
2019-11-14 16:14:52 -0700

    beginning containerized ldms testing

--- pipelines/data_generator/run.sh
+++ pipelines/data_generator/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # [wf] Run dummy simulation
-qsub -I -x -l nodes=16:ppn=8 -l walltime=2:00:00 -S /bin/bash -j oe -N cdse_datagen ${PWD}/run/datagen.sh
+qsub -I -x -q debug -l nodes=1:ppn=8 -l walltime=0:10:00 -S /bin/bash -j oe -N cdse_datagen ${PWD}/run/datagen.sh
--- pipelines/data_generator/setup.sh
+++ pipelines/data_generator/setup.sh
@@ -4,8 +4,8 @@
 module load openmpi-3.1.4-gcc-4.8.5-4rs6enq
 module load openssl-1.1.1b-gcc-4.8.5-obdqvnl
 module load singularity-3.2.1-gcc-4.8.5-ulix7vo
-rm -f run/bsp_prototype_latest.sif
+rm -f run/bsp_prototype_carc-wheeler.sif
 pushd run
-singularity pull docker://unmcarc/bsp_prototype:latest
+singularity pull docker://qwofford/bsp_prototype:carc-wheeler
 popd
 #fi
