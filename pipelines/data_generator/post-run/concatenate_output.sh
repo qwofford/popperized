5e7a10c2afe1330c2672e7c8236f1a4a388f73c2
Quincy Wofford <quincy.wofford@gmail.com>
2019-11-15 08:53:11 -0700

    move ldms outputs to repo

--- pipelines/data_generator/post-run/concatenate_output.sh
+++ pipelines/data_generator/post-run/concatenate_output.sh
@@ -2,4 +2,9 @@
 cat results/bsp_prototype.header > results/result.csv
 TMPDIR=$(cat /wheeler/scratch/${USER}/TMPDIR)
 paste -d'\n' results/job_output/* >> results/result.csv
+
+# Grab ldms output
+mv /wheeler/scratch/$(whoami)/ldms_output/* results/ldms_output/
+
 rm results/job_output/*
+rm /wheeler/scratch/$(whoami)/ldms_output/*
